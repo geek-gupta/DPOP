a18262d6bbb7c5fbe8bcb4853d2b812a231ea7fc
Gaurav Kumar <KIT9106@KITHYD.COM>
2019-03-12 17:44:18 +0530

    Add project in process

--- app/src/main/java/com/example/d_pop/activity/AddProjectActivity.java
+++ app/src/main/java/com/example/d_pop/activity/AddProjectActivity.java
@@ -1,10 +1,12 @@
 package com.example.d_pop.activity;
 
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.d_pop.R;
+import com.jaredrummler.materialspinner.MaterialSpinner;
 
 public class AddProjectActivity extends AppCompatActivity {
 
@@ -17,6 +19,15 @@ public class AddProjectActivity extends AppCompatActivity {
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle("Add Project");
+
+        MaterialSpinner spinner = (MaterialSpinner) findViewById(R.id.mentor_spinner);
+        spinner.setItems("Ice Cream Sandwich", "Jelly Bean", "KitKat", "Lollipop", "Marshmallow");
+        spinner.setOnItemSelectedListener(new MaterialSpinner.OnItemSelectedListener<String>() {
+
+            @Override public void onItemSelected(MaterialSpinner view, int position, long id, String item) {
+                Snackbar.make(view, "Clicked " + item, Snackbar.LENGTH_LONG).show();
+            }
+        });
     }
 
     @Override
